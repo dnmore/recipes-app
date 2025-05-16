4ffc8c812fcd167db40a90057d1d1553ad23fe3f
Diana Moretti <morettidn@gmail.com>
2025-05-16 14:31:16 +0200

    update homepage styling

---
+++ app/components/welcome/welcome.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router";
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center p-8 z-10">
+      <div className="flex-1 flex flex-col items-center gap-5 min-h-[450px] p-6 bg-hero bg-cover bg-no-repeat bg-center rounded-sm shadow-lg">
+        <header className="flex flex-col items-center gap-2 p-6 text-center ">
+          <h1 className="font-serif text-2xl md:text-4xl font-bold tracking-tight">
+            Discover delicious recipes in few clicks.
+          </h1>
+        </header>
+        <NavLink
+          to="/categories"
+          aria-label="Browse recipe categories"
+          className="px-8 py-2 border rounded-sm border-black bg-transparent text-lg font-bold uppercase hover:bg-black hover:text-white"
+        >
+          Get Started
+        </NavLink>
+      </div>
+    </main>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,15 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../components/welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "MyDish | Recipes, How-Tos and More" },
+    { name: "description", content: "Welcome to MyDish!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return  <Welcome />
+    
+   
 }
